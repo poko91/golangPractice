ca768ad5f5eb196fe020acd3eba98738b6c7f288
Poorna <kamatpoorna@gmail.com>
2021-09-09 17:53:30 +0530

    DataStructures: add more examples of maps

--- GolangPractice-02-DataStructures/maps.go
+++ GolangPractice-02-DataStructures/maps.go
@@ -3,14 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	// To create an empty map use syntax - make(map[key-type]val-type)
+
+	// Declare an empty map
+	var person = map[string]int{}
+	fmt.Println("1. Empty map created having string as key-type and int as value-type:")
+	fmt.Println(person)
+
+	// Map declaration using make function
 	m := make(map[string]int)
 
-	// To add more key-value pairs to the map use syntax: m[key] = value
+	// Adding key-value pairs to the map
 	m["Agnes"] = 78
 	m["Richard"] = 12
 	m["Alison"] = 24
 	m["Dennis"] = 18
+	fmt.Println("\n2. Adding items (key-value pairs) to the map:")
 	fmt.Println(m)
 
 	// You can also declare and initialize a new map in the same line with this syntax: (map[key-type]val-type){}
@@ -20,13 +27,37 @@ func main() {
 		"Roberto":    57,
 		"Alessandro": 46,
 	}
+	fmt.Println("\n3. Declare and initialize a new map in the same line:")
 	fmt.Println(nm)
 
 	// The builtin delete removes key/value pairs from a map.
-	delete(m, "Agnes")
-	delete(nm, "Ted")
+	fmt.Println("\n4. Deleting items from a map:")
+	fmt.Println("Before deleting a value:", m)
 
+	delete(m, "Agnes")
 	fmt.Println("After deleting a value:", m)
-	fmt.Println("After deleting a value:", nm)
+
+	// Accessing Items of a map
+	fmt.Println("\n5. Accessing specific items from a map:")
+	fmt.Println(nm["Maria"])
+
+	//Adding elements to an existing map
+	fmt.Println("\n6. Adding items to a map:")
+
+	m["Rocky"] = 30 // Add element
+	m["Joseph"] = 40
+	fmt.Println(m)
+
+	// Changing values of existing map items
+	fmt.Println("\n7. Changing values in a map:")
+	m["Richard"] = 84
+	m["Dennis"] = 12
+	fmt.Println(m) // Edit item
+
+	// Iterate over a Map
+	fmt.Println("\n8. Iterating over a map:")
+	for key, value := range m {
+		fmt.Println(key, "--", value)
+	}
 
 }
