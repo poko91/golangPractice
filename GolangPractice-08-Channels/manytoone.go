24100da95533ec9b9b9019d1efb9de5995605040
Poorna Kamat <82212882+poko91@users.noreply.github.com>
2021-10-31 23:21:45 +0530

    Channels : many_to_one add notes

--- GolangPractice-08-Channels/manytoone.go
+++ GolangPractice-08-Channels/manytoone.go
@@ -5,18 +5,21 @@ import (
 	"sync"
 )
 
+// many function writing to the same channel
 func main() {
 	c := make(chan int)
 	var wg sync.WaitGroup
 	wg.Add(2)
 
+	//func 01
 	go func() {
 		for i := 0; i < 10; i++ {
 			c <- i
 		}
 		wg.Done()
 	}()
-
+	
+	//func 02
 	go func() {
 		for i := 10; i < 20; i++ {
 			c <- i
