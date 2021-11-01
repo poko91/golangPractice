c977d0eb8c81be2442f285c75d507486d7b57e2a
Poorna <kamatpoorna@gmail.com>
2021-11-01 14:51:42 +0530

    Channels: Add an example of pipelines - refactor

--- GolangPractice-08-Channels/pipeline.go
+++ GolangPractice-08-Channels/pipeline.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	c := gen(6, 8, 9, 3, 10, 5, 78, 32, 17)
-	out := sq(c)
-	for n := range out {
+	// c := gen(6, 8, 9, 3, 10, 5, 78, 32, 17)
+	// out := sq(c)
+	// for n := range out {
+	// 	fmt.Println(n)
+	// }
+
+	for n := range sq(sq(gen(2, 3))) {
 		fmt.Println(n)
 	}
-
 }
 
 func gen(nums ...int) chan int {
