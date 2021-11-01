96583f7ba06f1387e45c24e146722c56aa34cc16
Poorna <kamatpoorna@gmail.com>
2021-11-01 14:43:17 +0530

    Channels: Add an example of pipelines using channels

---
+++ GolangPractice-08-Channels/pipeline.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := gen(6, 8, 9, 3, 10, 5, 78, 32, 17)
+	out := sq(c)
+	for n := range out {
+		fmt.Println(n)
+	}
+
+}
+
+func gen(nums ...int) chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in chan int) chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
