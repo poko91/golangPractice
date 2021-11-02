3231ca009d0d0dc120432e2881bcfcb8dc3991b5
Poorna <kamatpoorna@gmail.com>
2021-11-02 11:45:10 +0530

    Testing: Add example of Table Test

--- GolangPractice-10-Testing/main_test.go
+++ GolangPractice-10-Testing/main_test.go
@@ -3,8 +3,23 @@ package main
 import "testing"
 
 func TestMySum(t *testing.T) {
-	x := mySum(26, 19)
-	if x != 45 {
-		t.Error("Expected", 45, "Got", x)
+
+	type test struct {
+		data   []int
+		answer int
+	}
+
+	tests := []test{
+		test{[]int{21, 21}, 42},
+		test{[]int{1, 1}, 2},
+		test{[]int{3, 4, 5}, 12},
+		test{[]int{-1, 0, 1}, 0},
+	}
+
+	for _, v := range tests {
+		x := mySum(v.data...)
+		if x != v.answer {
+			t.Error("Expected", v.answer, "Got", x)
+		}
 	}
 }
