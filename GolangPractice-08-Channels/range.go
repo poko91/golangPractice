2f949c775eab5b94f71ccc0cfb026dbbd153dabd
Poorna <kamatpoorna@gmail.com>
2021-11-05 23:42:00 +0530

    Channels: Code for ranging over data from a Channel

--- GolangPractice-08-Channels/range.go
+++ GolangPractice-08-Channels/range.go
@@ -1,39 +1,5 @@
 package main
 
-<<<<<<< HEAD
-import (
-	"fmt"
-	"sync"
-)
-
-func main() {
-	c := make(chan int)
-	var wg sync.WaitGroup
-
-	go func() {
-		wg.Add(1)
-		for i := 0; i < 10; i++ {
-			c <- i
-		}
-		wg.Wait()
-		close(c)
-	}()
-
-	go func() {
-		wg.Add(1)
-		for i := 10; i < 20; i++ {
-			c <- i
-		}
-		wg.Wait()
-		close(c)
-	}()
-
-	go func(){
-		wg.Done()
-		close(c)
-	}
-
-=======
 import "fmt"
 
 func main() {
@@ -46,7 +12,6 @@ func main() {
 		close(c)
 	}()
 
->>>>>>> e00d3f0b091a0350fbff80433171d9ecfc077c8f
 	for n := range c {
 		fmt.Println(n)
 	}
